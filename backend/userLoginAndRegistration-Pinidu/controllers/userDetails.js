de45c8490e9c3c90191c155123dc8342a4ab0f02
Pinidu557 <piniduransika961@gmail.com>
2025-08-19 22:33:58 +0530

    Completed backend part of login and registration

---
+++ backend/userLoginAndRegistration-Pinidu/controllers/userDetails.js
@@ -0,0 +1,21 @@
+import userModel from "../models/userModel.js";
+
+export const getUserDetails = async (req, res) => {
+  try {
+    const { userId } = req.userId;
+    const user = await userModel.findOne({ userId });
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+    return res.json({
+      success: true,
+      userData: {
+        name: user.name,
+        email: user.email,
+        isAccountVerified: user.isAccountVerified,
+      },
+    });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
---
+++ backend/userLoginAndRegistration-Pinidu/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv/config";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
+import userRouter from "./Routes/userRoutes.js";
+import userDetailsRouter from "./Routes/userDetailsRoutes.js";
+
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ credentials: true }));
+
+//API Endpoints
+app.get("/", (req, res) => res.send("API Worrking ON Fire"));
+app.use("/api/auth", userRouter);
+app.use("/api/user", userDetailsRouter);
+
+app.listen(port, () => console.log(`server started on PORT:${port}`));
