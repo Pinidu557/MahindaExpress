ff887006a10baba18c6706ce78358ff80afdea79
oshadha kalhara <oshadhawedaguruge@gmail.com>
2025-09-06 09:19:02 +0530

    Restore original frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,18 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import RoutesPage from "./pages/RoutesPage";
 import VehiclesPage from "./pages/VehiclesPage";
-import PassengerHome from "./pages/PassengerHome";
-import PassengerLogin from "./pages/PassengerLogin";
-import PassengerEmailVerify from "./pages/PassengerEmailVerify";
-import PassengerRestPassword from "./pages/PassengerRestPassword";
-import PassengerJourney from "./pages/PassengerJourney";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
-export const App = () => {
+function App() {
   return (
-    <div>
-      <ToastContainer />
+    <Router>
       <Routes>
-        <Route path="/" element={<PassengerHome />} />
-        <Route path="/login" element={<PassengerLogin />} />
-        <Route path="/email-verify" element={<PassengerEmailVerify />} />
-        <Route path="/reset-password" element={<PassengerRestPassword />} />
-        <Route path="/journeys" element={<PassengerJourney />} />
         <Route path="/" element={<Dashboard />} />
         <Route path="/routes" element={<RoutesPage />} />
         <Route path="/vehicles" element={<VehiclesPage />} />
       </Routes>
-    </div>
+    </Router>
   );
-};
+}
 
 export default App;
