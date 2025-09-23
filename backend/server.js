67b108f4179e5617fa9183ede2a0c993617a8336
oshadha kalhara <oshadhawedaguruge@gmail.com>
2025-09-23 08:36:03 +0530

    2025.9.23
    latest commit

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,10 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv/config";
-import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
-import userRouter from "./Routes/userRoutes.js";
-import userDetailsRouter from "./Routes/userDetailsRoutes.js";
 import routeRoutes from "./routes/routeRoutes.js";
 import vehicleRoutes from "./routes/vehicalRoutes.js";
+import cookieParser from "cookie-parser"; // For ES modules
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -20,8 +18,6 @@ app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 //API Endpoints
 app.get("/", (req, res) => res.send("API Worrking ON Fire"));
-app.use("/api/auth", userRouter);
-app.use("/api/user", userDetailsRouter);
 app.use("/api/routes", routeRoutes);
 app.use("/api/vehicles", vehicleRoutes);
 
