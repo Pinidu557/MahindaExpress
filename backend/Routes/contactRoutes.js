c7f465e42fab4b320423d697b0a0e92842eec559
Pinidu557 <piniduransika961@gmail.com>
2025-10-16 09:10:25 +0530

    refresh main 10.16

--- backend/Routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import contactUs, { getContactMessages } from "../controllers/contacsController.js";
-
-const contactRouter = express.Router();
-
-contactRouter.post("/contactus", contactUs);
-contactRouter.get("/", getContactMessages);
-
-export default contactRouter;
--- frontend/src/components/index.js
+++
@@ -1,9 +0,0 @@
-// Export all components from a single file for easy importing
-export { default as LoadingSpinner } from './LoadingSpinner';
-export { default as EmptyState } from './EmptyState';
-export { default as StatusBadge } from './StatusBadge';
-export { default as AnimatedCard } from './AnimatedCard';
-export { default as Button } from './Button';
-export { default as Input } from './Input';
-export { default as Modal } from './Modal';
-
